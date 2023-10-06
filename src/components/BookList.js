de37e39e70eb8ff9d5e0bbbacd3ab61a960bb049
Pratiksha-Trivedi <pratiksha.trivedi@innogent.in>
2023-10-06 18:20:29 +0530

    Completed

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -9,9 +9,9 @@ import { Link, useParams } from 'react-router-dom';
 const BookList = () => {
   const dispatch = useDispatch();
   const books = useSelector(state => state.books);
-  console.log(books)
   const [editingBook, setEditingBook] = useState({ id: '', name: '', author: '' });
   const [selectedBook, setSelectedBook] = useState(null);
+  const [filterText, setFilterText] = useState('');
   const { libraryId } = useParams(); // Extract libraryId from URL
 
   const fetchData = async () => {
@@ -68,6 +68,14 @@ const BookList = () => {
   return (
     <div>
       <h2>Book List</h2>
+      <div>
+        <input
+          type="text"
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+          placeholder="Filter by name..."
+        />
+      </div>
       <table className='table'>
         <thead>
           <tr>
@@ -78,29 +86,31 @@ const BookList = () => {
           </tr>
         </thead>
         <tbody>
-          {books && books.map(book => (
-            editingBook.id === book.id ? (
-              <tr key={book.id}>
-                <td>{book.id}</td>
-                <td><input type="text" value={editingBook.name} onChange={(event) => setEditingBook({ ...editingBook, name: event.target.value })} /></td>
-                <td><input type="text" value={editingBook.author} onChange={(event) => setEditingBook({ ...editingBook, author: event.target.value })} /></td>
-                <td>
-                  <button onClick={() => handleUpdateBook(book.id)} className="btn btn-success">Update</button>
-                  <button onClick={() => setEditingBook({ id: '', name: '', author: '' })} className="btn btn-warning">Back</button>
-                </td>
-              </tr>
-            ) : (
-              <tr key={book.id}>
-                <td>{book.id}</td>
-                <td>{book.name}</td>
-                <td>{book.author}</td>
-                <td>
-                  <button onClick={() => handleEditBook(book)}>Edit</button>
-                  <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
-                </td>
-              </tr>
-            )
-          ))}
+          {books
+            .filter(book => book.name.toLowerCase().startsWith(filterText.toLowerCase()))
+            .map(book => (
+              editingBook.id === book.id ? (
+                <tr key={book.id}>
+                  <td>{book.id}</td>
+                  <td><input type="text" value={editingBook.name} onChange={(event) => setEditingBook({ ...editingBook, name: event.target.value })} /></td>
+                  <td><input type="text" value={editingBook.author} onChange={(event) => setEditingBook({ ...editingBook, author: event.target.value })} /></td>
+                  <td>
+                    <button onClick={() => handleUpdateBook(book.id)} className="btn btn-success">Update</button>
+                    <button onClick={() => setEditingBook({ id: '', name: '', author: '' })} className="btn btn-warning">Back</button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={book.id}>
+                  <td>{book.id}</td>
+                  <td>{book.name}</td>
+                  <td>{book.author}</td>
+                  <td>
+                    <button onClick={() => handleEditBook(book)}>Edit</button>
+                    <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            ))}
         </tbody>
       </table>
     </div>
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -11,6 +11,7 @@ const LibraryList = () => {
   const libraries = useSelector(state => state.libraries);
   const [editingLibrary, setEditingLibrary] = useState({ id: '', name: '', ownerName: '', books: [] });
   const [selectedLibrary, setSelectedLibrary] = useState(null);
+  const [filterText, setFilterText] = useState('');
 
   const fetchData = async () => {
     try {
@@ -64,6 +65,14 @@ const LibraryList = () => {
   return (
     <div>
       <h2>Library List</h2>
+      <div>
+        <input
+          type="search"
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+          placeholder="Search by library name..."
+        />
+      </div>
       <table className='table'>
         <thead>
           <tr>
@@ -74,31 +83,33 @@ const LibraryList = () => {
           </tr>
         </thead>
         <tbody>
-          {libraries && libraries.map(library => (
-            editingLibrary.id === library.id ? (
-              <tr key={library.id}>
-                <td>{library.id}</td>
-                <td><input type="text" value={editingLibrary.name} onChange={(event) => setEditingLibrary({ ...editingLibrary, name: event.target.value })} /></td>
-                <td><input type="text" value={editingLibrary.ownerName} onChange={(event) => setEditingLibrary({ ...editingLibrary, ownerName: event.target.value })} /></td>
-                <td>
-                  <button onClick={() => handleUpdateLibrary(library.id)} className="btn btn-success">Update</button>
-                  <button onClick={() => setEditingLibrary({ id: '', name: '', ownerName: '', books: [] })} className="btn btn-warning">Back</button>
-                </td>
-              </tr>
-            ) : (
-              <tr key={library.id}>
-                <td>{library.id}</td>
-                <td>{library.name}</td>
-                <td>{library.ownerName}</td>
-                <td>
-                  <button><Link to={`/addBook/${library.id}`}  className="btn btn-sm btn-info" >Add Book</Link></button>
-                  <button><Link to={`/books/${library.id}`}>Show Books</Link></button>
-                  <button onClick={() => handleEditLibrary(library)}>Edit</button>
-                  <button onClick={() => handleDeleteLibrary(library.id)}>Delete</button>
-                </td>
-              </tr>
-            )
-          ))}
+          {libraries
+            .filter(library => library.name.toLowerCase().startsWith(filterText.toLowerCase()))
+            .map(library => (
+              editingLibrary.id === library.id ? (
+                <tr key={library.id}>
+                  <td>{library.id}</td>
+                  <td><input type="text" value={editingLibrary.name} onChange={(event) => setEditingLibrary({ ...editingLibrary, name: event.target.value })} /></td>
+                  <td><input type="text" value={editingLibrary.ownerName} onChange={(event) => setEditingLibrary({ ...editingLibrary, ownerName: event.target.value })} /></td>
+                  <td>
+                    <button onClick={() => handleUpdateLibrary(library.id)} className="btn btn-success">Update</button>
+                    <button onClick={() => setEditingLibrary({ id: '', name: '', ownerName: '', books: [] })} className="btn btn-warning">Back</button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={library.id}>
+                  <td>{library.id}</td>
+                  <td>{library.name}</td>
+                  <td>{library.ownerName}</td>
+                  <td>
+                    <button><Link to={`/addBook/${library.id}`}  className="btn btn-sm btn-info" >Add Book</Link></button>
+                    <button><Link to={`/books/${library.id}`}>Show Books</Link></button>
+                    <button onClick={() => handleEditLibrary(library)}>Edit</button>
+                    <button onClick={() => handleDeleteLibrary(library.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            ))}
         </tbody>
       </table>
     </div>
